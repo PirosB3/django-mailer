b25dc3a958ced7daab33609ecec2044267094dc0
Daniel Pyrathon <pirosb3@gmail.com>
2015-04-12 16:23:12 +0200

    Implemented django forms in admin

--- mailchecker/admin.py
+++ mailchecker/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
+
 from .models import Thread, Message
+from .forms import MessageForm, MessageInlineForm
 
 
 class MessageInline(admin.TabularInline):
     model = Message
+    form = MessageInlineForm
 
 
 class MessageAdmin(admin.ModelAdmin):
     ordering = ('id',)
     model = Message
+    form = MessageForm
 
 
 class ThreadAdmin(admin.ModelAdmin):
